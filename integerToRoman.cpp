2131033bb7fc2ecb81b73025b5c27a5b0fe50b21
Leon <goodnight327@gmail.com>
2022-09-24 12:09:32 +0800

    roman <-> integer

---
+++ integerToRoman.cpp
@@ -0,0 +1,93 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string s;   //储存最终的罗马数字
+        //string n = to_string(num);
+        //int size = n.size();
+        int rem = 0;   //储存余数
+        int indice = 0;   //工具数字
+
+        //计算千位数
+        if (num >= 1000) {
+            rem = num % 1000;
+            indice = (num - rem) / 1000;
+            s.append(indice, 'M');
+            num = rem;
+        }
+        
+
+        //计算百位数
+        if (num >= 900) {
+            s.append("CM");
+            num -= 900;
+        }
+        else if (num >= 500) {
+            s.append("D");
+            num -= 500;
+            if (num >= 100) {
+                rem = num % 100;
+                indice = (num - rem) / 100;
+                s.append(indice, 'C');
+                num = rem;
+            }
+        }
+        else if (num >= 100) {
+            rem = num % 100;
+            indice = (num - rem) / 100;
+            s.append(indice, 'C');
+            num = rem;
+        }
+
+        //计算十位数
+        if (num >= 90) {
+            s.append("XC");
+            num -= 90;
+        }
+        else if (num >= 50) {
+            s.append("L");
+            num -= 50;
+            if (num >= 10) {
+                rem = num % 10;
+                indice = (num - rem) / 10;
+                s.append(indice, 'X');
+                num = rem;
+            }
+        }
+        else if (num >= 10) {
+            rem = num % 10;
+            indice = (num - rem) / 10;
+            s.append(indice, 'X');
+            num = rem;
+        }
+
+        //计算个位数
+        if (num == 9) {
+            s.append("IX");
+        }
+        else if (num >= 5) {
+            s.append("V");
+            num -= 5;
+            s.append(num, 'I');
+        }
+        else if (num == 4) {
+            s.append("IV");
+        }
+        else {
+            s.append(num, 'I');
+        }
+
+        return s;
+    }
+};
+
+int main() {
+    Solution sol;
+    sol.intToRoman(3);
+
+    return 0;
+}
--- romanToInteger.cpp
+++ romanToInteger.cpp
@@ -1,18 +1,110 @@
 #include <string>
 #include <vector>
+#include <iostream>
 using namespace std;
 
 
 class Solution {
 public:
     int romanToInt(string s) {
-        vector<int> indice(7); //M, D, C, L, X, V, I
-        for (size_t i=0; i<s.size(); i++) {
-            switch (s[i]) {
-                case 'M':
-                    indice[0] += 1;
-                    break;
+       int iniPoint = 0;   //循环开始的起点
+       int size = s.size();
+       int value = 0;   // s代表的值
+       
+       //千位数计算
+       if (s[0] == 'M') {
+        for (size_t i = iniPoint; s[i] == 'M'; i++) {
+            value += 1000;
+            iniPoint += 1;
+        }
+       }
+
+       //百位数计算 
+       if (s[iniPoint] == 'C') {
+        if (s[iniPoint+1] == 'M') {
+            value += 900;
+            iniPoint += 2;
+        }
+        else if (s[iniPoint+1] == 'D') {
+            value += 400;
+            iniPoint += 2;
+        }
+        else {
+            for (size_t i = iniPoint; s[i] == 'C'; i++) {
+                value += 100;
+                iniPoint += 1;
             }
         }
+       }
+       else if (s[iniPoint] == 'D') {
+        value += 500;
+        iniPoint += 1;
+        for (size_t i = iniPoint; s[i] == 'C'; i++) {
+            value += 100;
+            iniPoint += 1;
+        }
+       }
+
+       //十位数计算
+       if (s[iniPoint] == 'X') {
+        if (s[iniPoint + 1] == 'C') {
+            value += 90;
+            iniPoint += 2;
+        }
+        else if (s[iniPoint + 1] == 'L') {
+            value += 40;
+            iniPoint += 2;
+        }
+        else {
+            for (size_t i = iniPoint; s[i] == 'X'; i++) {
+                value += 10;
+                iniPoint += 1;
+            }
+        }
+       }
+       else if (s[iniPoint] == 'L') {
+        value += 50;
+        iniPoint += 1;
+        for (size_t i = iniPoint; s[i] == 'X'; i++) {
+            value += 10;
+            iniPoint += 1;
+        }
+       }
+
+       //个位数计算
+       if (s[iniPoint] == 'I') {
+        if (s[iniPoint + 1] == 'X') {
+            value += 9;
+            iniPoint += 2;
+        }
+        else if (s[iniPoint + 1] == 'V') {
+            value += 4;
+            iniPoint += 2;
+        }
+        else {
+            for (size_t i = iniPoint; s[i] == 'I'; i++) {
+                value += 1;
+                iniPoint += 1;
+            }
+        }
+       }
+       else if (s[iniPoint] == 'V') {
+        value += 5;
+        iniPoint += 1;
+        for (size_t i = iniPoint; s[i] == 'I'; i++) {
+            value += 1;
+            iniPoint += 1;
+        }
+       }
+
+       return value;
     }
 };
+ 
+int main () {
+    Solution sol;
+    int integer = sol.romanToInt("MCMXCIV");
+    cout << integer;
+
+    return 0;
+}
